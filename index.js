a96680b8ff33026fcd12194c6fb0b1d2b96aad5b
Ennnm <en@DESKTOP-HB00MK8.localdomain>
2021-08-07 18:21:57 +0800

    base

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+import express from "express";
+import { read } from './jsonFileStorage.mjs'
+
+const app = express();
+
+const handleRecipes = (req, res)=> 
+{
+  read('data.json', (err, content)=>{
+    const recipeIdx = req.params.index;
+     if( recipeIdx < 0 || recipeIdx >= content.recipes.length)
+     {
+       res.status(404).send('Sorry, we cannot find that!');
+     }
+     else{
+        const recipe = content.recipes[recipeIdx];
+        res.send(recipe);
+     }
+
+  })
+}
+
+
+
+app.get('/recipe/:index', handleRecipes);
+app.listen(3004);
